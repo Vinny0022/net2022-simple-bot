5f5a189c0f886c5b6386eff9ea97091e5ba426fb
Vinícius Plaza <vini_a.m.plaza@hotmail.com>
2022-04-12 19:12:37 -0300

    adicionando Repository como padrão

--- SimpleBotCore/Repositories/QuestionMockRepository.cs
+++ SimpleBotCore/Repositories/QuestionMockRepository.cs
@@ -3,37 +3,13 @@ using System.Collections.Generic;
 
 namespace SimpleBotCore.Repositories
 {
-    public class QuestionMockRepository : IQuestionRepository
+    public class QuestionMockRepository //: IQuestionRepository
     {
         Dictionary<string, string> _questions = new Dictionary<string, string>();
         public void CadastrarPergunta(string pergunta)
         {
             _questions[pergunta] = pergunta;
         }
-
-        //private void SaveQuestion(Question question)
-        //{
-        //    _questions[question.Id] = question;
-        //}
-
-        //public Question TryLoadQuestion(string question)
-        //{
-        //    if (Exists(question))
-        //    {
-        //        return GetQuestion(question);
-        //    }
-
-        //    return null;
-        //}        
-
-        //private Question GetQuestion(string question)
-        //{
-        //    return _questions[question];
-        //}        
-
-        //private bool Exists(string question)
-        //{
-        //    return _questions.ContainsKey(question);
-        //}
+        
     }
 }
